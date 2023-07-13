06c0bd4f41102942a2c7373bba79a680f28c24b3
Pietro <rodriguespietro004@gmail.com>
2023-07-12 23:39:31 -0300

    first commit

---
+++ ex05.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func selectionSort(arr []int) {
+    n := len(arr)
+    for i := 0; i < n-1; i++ {
+        minIndex := i
+        for j := i + 1; j < n; j++ {
+            if arr[j] < arr[minIndex] {
+                minIndex = j
+            }
+        }
+        arr[i], arr[minIndex] = arr[minIndex], arr[i]
+    }
+}
+
+func main() {
+    arr := []int{64, 25, 12, 22, 11}
+    fmt.Println("Array antes da ordenação:", arr)
+
+    selectionSort(arr)
+
+    fmt.Println("Array após a ordenação:", arr)
+}
